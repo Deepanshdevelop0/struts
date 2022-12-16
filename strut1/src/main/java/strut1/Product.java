f58c2b46d019602883c8185a005c3cbd52fbd41e
Deepanshdevelop0 <99035471+Deepanshdevelop0@users.noreply.github.com>
2022-12-16 15:27:57 +0530

    Uploaded

---
+++ strut1/src/main/java/strut1/Product.java
@@ -0,0 +1,17 @@
+package strut1;
+
+public class Product {
+	private String name;
+	
+	public String getName() {
+		return name;
+	}
+	
+	public void setName(String name) {
+		this.name = name;
+	}
+	
+	public String execute() {
+		return "Success";
+	}
+}
